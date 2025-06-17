1ce166e63021b82d4381588d072fe855df5e9a53
Andrii Novak <4tune.dev@gmail.com>
2025-06-17 22:31:18 +0300

    Add BookController class

---
+++ src/main/java/com/tune/library/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.tune.library.controller;
+
+import com.tune.library.dto.BookDTO;
+import com.tune.library.entity.Book;
+import com.tune.library.service.BookService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    // Book creation
+    @PostMapping
+    public ResponseEntity<Book> createBook(@Valid @RequestBody BookDTO bookDTO) {
+        Book createdBook = bookService.createBook(bookDTO);
+        return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+    }
+}

01ebf077d7536e6addd4c92cc802fb1772c6696d
Andrii Novak <4tune.dev@gmail.com>
2025-07-21 21:05:20 +0300

    Update WebController with possibility to add new book

--- src/main/java/com/tune/library/controller/WebController.java
+++ src/main/java/com/tune/library/controller/WebController.java
@@ -1,10 +1,13 @@
 package com.tune.library.controller;
 
+import com.tune.library.dto.BookDTO;
 import com.tune.library.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -16,5 +19,17 @@ public class WebController {
     public String showBooks(Model model) {
         model.addAttribute("books", bookService.getAllBooks());
         return "books";
+    }
+
+    @GetMapping("/books/new")
+    public String showAddForm(Model model) {
+        model.addAttribute("bookDTO", new BookDTO());
+        return "add-book";
+    }
+
+    @PostMapping("/books")
+    public String addBook(@ModelAttribute BookDTO bookDTO) {
+        bookService.createBook(bookDTO);
+        return "redirect:/books";
     }
 }
